3fdbd473fb67dbf2e025b71db7f78913a8e26ec5
YuuliiaS <124794043+YuuliiaS@users.noreply.github.com>
2023-03-30 15:00:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Завтрак в 09:00";
+            var regex = new Regex(@"\b(?:[01][0-9]|2[0-3]):[0-5][0-9]\b");
+            var matches = regex.Matches(a).Cast<Match>().Select(x => x.Value);
+            foreach (var match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
